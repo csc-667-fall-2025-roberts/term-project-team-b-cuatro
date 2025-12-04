9ed50a6979308976e2f801d0cb82b151dc13eab7
flor-et <jashernandez5704@gmail.com>
2025-12-04 13:33:49 -0800

    fixes

--- src/backend/routes/root.ts
+++ src/backend/routes/root.ts
@@ -1,14 +1,25 @@
+// imports
 import express from "express";
 import path from "path";
+import { pool } from "../databases/db";
 
+// routing to index
 const router = express.Router();
-
 router.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/index.html"));
 });
-
 router.post("/", (_req, res) => {
   res.send("you posted");
 });
+
+// // api endpoints
+// router.get("/games", async (_req, res,) => {
+//   try{
+//     const result = await pool.query("SELECT * FROM games");
+//     res.json(result.rows)
+//   } catch (err){
+//     res.status(500);
+//   }
+// });
 
 export default router;
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,29 +1,35 @@
+// package imports
 import * as path from "path";
 import express from "express";
 import morgan from "morgan";
 import createHttpError from "http-errors";
 
+// file imports
 import rootRoutes from "./routes/root";
 import { testRouter } from "./routes/test";
 
+// create express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware setup
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
+// routing
 app.use("/", rootRoutes);
 app.use("/test", testRouter);
 
+// error handling
 app.use((_request, _response, next) => {
   next(createHttpError(404));
 });
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
